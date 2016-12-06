cd91976566f63582e16731ddab97e2d5efe07cb2
Martin Brzenska <martin@mab-on.net>
2016-12-06 20:32:42 +0100

    Refactoring

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ usage & examples
 This example shows a query for a-tags, that are children of a li-tag and has a class attibute with the value "link".
 We want to the output to be "Tag"\t"Element attributes csv"\t"value of the element ettribute href"\n for each hit
 ```sh
-$ cat ./dummy.html | ./dominator -f'li.a{class:link}' -o'tag' -o'attrib-keys' -o'attrib(href)'
+$ cat ./dummy.html | ./dominator 'li.a{class:link}' -o'tag' -o'attrib-keys' -o'attrib(href)'
 a	href,id,class	#a-1-li-1-o2-1
 a	href,id,class	#a-2-li-2-o2-1
 a	href,id,class	#a-3-li-2-o2-1
@@ -43,7 +43,7 @@ a	href,id,class	#a-3-li-2-o2-1
 
 This Example shows a query for a-tags where the href begins with "http"
 ```sh
-$ cat ./dummy.html | ./dominator -f'a{href:(regex)^http}' -o'tag' -o'attrib-keys' -o'attrib(href)'
+$ cat ./dummy.html | ./dominator 'a{href:(regex)^http}' -o'tag' -o'attrib-keys' -o'attrib(href)'
 a	href,id,class	https://github.com
 ```
 
--- source/app.d
+++ source/app.d
@@ -30,65 +30,79 @@ int main(string[] args)
         withComments,
         squashWhitespaces;
 
-    auto optResult = getopt(
-        args,
-        "filter|f",
-        "\nA Dominator specific filter expression.\n"
-        ~"Examples:\n"
-        ~"\t" ~ "div" ~ "\t\t\t" ~ "div nodes." ~ "\n"
-        ~"\t" ~ "div.p" ~ "\t\t\t" ~ "p nodes which are direct children of a div." ~ "\n"
-        ~"\t" ~ "div[1].p" ~ "\t\t" ~ "p nodes which are direct children of the first div in the document." ~ "\n"
-        ~"\t" ~ "div[1..3].p{id:wanted}" ~ "\t" ~ "p nodes with the attribute id='wanted', which are direct children of the first three divs in the document." ~ "\n"
-        ~"\t" ~ "div.*{id:wanted}" ~ "\t" ~ "All nodes with the attribute id='wanted' which are direct children of a div." ~ "\n"
-        ~"\t" ~ "input{checked:}" ~ "\t\t" ~ "All input nodes with the attribute 'checked'." ~ "\n"
-        ~"\t" ~ "a{href:(regex)^http://}" ~ "\t" ~ "a-nodes with a href attribute that matches the regular expression '^http://'." ~ "\n"
-        ~"\t" ~ "i{class:(regex)link}" ~ "\t" ~ "i nodes with 'link' as or inside the attribute 'class'." ~ "\n"
-        , &optTags,
+    GetoptResult optResult;
+    try {
+        optResult = getopt(
+            args,
+            "filter|f",
+            "\nA Dominator specific filter expression.\n"
+            ~"Examples:\n"
+            ~"\t" ~ "div" ~ "\t\t\t" ~ "div nodes." ~ "\n"
+            ~"\t" ~ "div.p" ~ "\t\t\t" ~ "p nodes which are direct children of a div." ~ "\n"
+            ~"\t" ~ "div[1].p" ~ "\t\t" ~ "p nodes which are direct children of the first div in the document." ~ "\n"
+            ~"\t" ~ "div[1..3].p{id:wanted}" ~ "\t" ~ "p nodes with the attribute id='wanted', which are direct children of the first three divs in the document." ~ "\n"
+            ~"\t" ~ "div.*{id:wanted}" ~ "\t" ~ "All nodes with the attribute id='wanted' which are direct children of a div." ~ "\n"
+            ~"\t" ~ "input{checked:}" ~ "\t\t" ~ "All input nodes with the attribute 'checked'." ~ "\n"
+            ~"\t" ~ "a{href:(regex)^http://}" ~ "\t" ~ "a-nodes with a href attribute that matches the regular expression '^http://'." ~ "\n"
+            ~"\t" ~ "i{class:(regex)link}" ~ "\t" ~ "i nodes with 'link' as or inside the attribute 'class'." ~ "\n"
+            , &optTags,
 
-        "output-item|o",
-        "\nDefines the output. Valid arguments are:\n"
-        ~"\t" ~ "tag" ~"\t\t" ~ "The name of the node" ~ "\n"
-        ~"\t" ~ "element-opener" ~"\t" ~ "The opening node-tag." ~ "\n"
-        ~"\t" ~ "element" ~"\t\t" ~ "The nodes full content." ~ "\n"
-        ~"\t" ~ "element-inner" ~"\t" ~ "The nodes full inner content." ~ "\n"
-        ~"\t" ~ "element-strip" ~"\t" ~ "The nodes full inner content without tags." ~ "\n"
-        ~"\t" ~ "element-start" ~"\t" ~ "The position of the opening tag in the element." ~ "\n"
-        ~"\t" ~ "element-end" ~"\t" ~ "The position of the termination tag in the element." ~ "\n"
-        ~"\t" ~ "attrib-keys" ~"\t" ~ "A comma-separated list of the nodes attributes." ~ "\n"
-        ~"\t" ~ "attrib(ATTRIB)" ~"\t" ~ "The value of the attribute ATTRIB of the node. " ~ "\n"
-        , &optOutItems,
+            "output-item|o",
+            "\nDefines the output. Valid arguments are:\n"
+            ~"\t" ~ "tag" ~"\t\t" ~ "The name of the node" ~ "\n"
+            ~"\t" ~ "element-opener" ~"\t" ~ "The opening node-tag." ~ "\n"
+            ~"\t" ~ "element" ~"\t\t" ~ "The nodes full content." ~ "\n"
+            ~"\t" ~ "element-inner" ~"\t" ~ "The nodes full inner content." ~ "\n"
+            ~"\t" ~ "element-strip" ~"\t" ~ "The nodes full inner content without tags." ~ "\n"
+            ~"\t" ~ "element-start" ~"\t" ~ "The position of the opening tag in the element." ~ "\n"
+            ~"\t" ~ "element-end" ~"\t" ~ "The position of the termination tag in the element." ~ "\n"
+            ~"\t" ~ "attrib-keys" ~"\t" ~ "A comma-separated list of the nodes attributes." ~ "\n"
+            ~"\t" ~ "attrib(ATTRIB)" ~"\t" ~ "The value of the attribute ATTRIB of the node. " ~ "\n"
+            , &optOutItems,
 
-        "output-item-terminator|t",
-        "\nCharacter, that terminates one item group on output\n",
-        &optNodeTerminator,
+            "output-item-terminator|t",
+            "\nCharacter, that terminates one item group on output\n",
+            &optNodeTerminator,
 
-        "output-item-serparator|s",
-        "\nCharacter, that separates the items on output\n",
-        &optNodeSeparator,
+            "output-item-serparator|s",
+            "\nCharacter, that separates the items on output\n",
+            &optNodeSeparator,
 
-        "input-file|i",
-        "\nRead the input from a file instead of stdin\n" ,
-        &inputFile,
+            "input-file|i",
+            "\nRead the input from a file instead of stdin\n" ,
+            &inputFile,
 
-        "with-html-comments|c",
-        "",
-        &withComments,
+            "with-html-comments|c",
+            "",
+            &withComments,
 
-        "squash-whitespaces|w",
-        "\nRemoves multiple whitespaces." ~ "\n"
-        ~"Only applies to the output-items 'element-strip' , 'element-inner' , 'element'" ~ "\n",
-        &squashWhitespaces
+            "squash-whitespaces|w",
+            "\nRemoves multiple whitespaces." ~ "\n"
+            ~"Only applies to the output-items 'element-strip' , 'element-inner' , 'element'" ~ "\n",
+            &squashWhitespaces
 
-    );
+        );
+    }
+    catch(GetOptException e) {
+        writefln("Something is wrong.\nSee '%s --help'", args[0]);
+        return 1;
+    }
 
     if(optResult.helpWanted)
     {
         defaultGetoptPrinter(
-            "\ndominator - Command-line HTML scraper.\n"
+            "\n"
+            ~"+========================================+\n"
+            ~"|                                        |\n"
+            ~"| dominator - Command-line HTML scraper. |\n"
+            ~"|                                        |\n"
+            ~"+========================================+\n"
+            ~"\n"
             ~"Author: Martin Brzenska (martin@mab-on.net)\n"
             ~"Copyright © 2016, Martin Brzenska\n"
 	        ~"License: MIT\n"
             ~"Donate: Paypal: martin.brzenska@gmail.com\n"
+            ~"\nUsage:\n\ndominator [FILTER-EXPRESSION] [PARAMETER]\n"
             ~"\nParameters:\n"
             ,
             optResult.options
@@ -123,6 +137,9 @@ int main(string[] args)
         }
     }
 
+    if(args.length == 2) {
+        optTags ~= args[1];
+    }
 
     auto domFilterHandler = DomFilter(optTags);
     auto dom = new Dominator(input);
